59013af60f1d2d8759292a566f4d287fcc75292e
Derek Gavey <dgavey@gavey.ca>
2022-01-12 13:52:10 -0700

    Adds some example actions

--- app/components/episode-list/component.js
+++ app/components/episode-list/component.js
@@ -1,8 +1,26 @@
 import Component from '@glimmer/component';
-// import { action } from '@ember/object';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class EpisodeList extends Component {
-  get episodes() {
+  @tracked episodeOnesOnly = false;
+
+  get allEpisodes() {
     return this.args.episodes;
+  }
+
+  get episodes() {
+    if (this.episodeOnesOnly) {
+      return this.allEpisodes.filterBy('episodeInSeason', 1);
+    }
+    return this.allEpisodes;
+  }
+
+  @action showOnlyEpisodeOnes() {
+    this.episodeOnesOnly = true;
+  }
+
+  @action showAll() {
+    this.episodeOnesOnly = false;
   }
 }
